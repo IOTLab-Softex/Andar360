8aa55306fb16ae603594f780c22d0571a6926136
Renato-Santos <renatolb8787@gmail.com>
2025-12-02 13:53:18 -0300

    correção do marca como vistas

--- app/helpers/visualizacao_helper.rb
+++ app/helpers/visualizacao_helper.rb
@@ -2,12 +2,11 @@
 module VisualizacaoHelper
 def marcar_como_visualizado(tipo, id)
   cookies["#{tipo}-#{id}"] = {
-    value: Time.current.to_i, # salva timestamp da visualização
+    value: Time.current.to_i,
     expires: 1.year.from_now,
     path: '/'
   }
 end
-
 
 def visualizado_pelo_usuario?(tipo, objeto)
   raw_cookie = cookies["#{tipo}-#{objeto.id}"]
@@ -16,6 +15,7 @@ def visualizado_pelo_usuario?(tipo, objeto)
   visualizado_em = Time.at(raw_cookie.to_i)
   visualizado_em >= objeto.updated_at
 end
+
 
 
 end
