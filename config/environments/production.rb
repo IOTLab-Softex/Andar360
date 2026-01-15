44cd648ebebed3a3dd2dbdb923191e6ef9db0dcb
Renato-Santos <renatolb8787@gmail.com>
2026-01-15 12:29:46 -0300

    correção do production.erb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "localhost"
-  config.hosts << "127.0.0.1"
   # Code is not reloaded between requests.
   config.enable_reloading = false
   
